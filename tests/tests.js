33bfeb355206292775113d25f5895e3306a06983
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-12 15:01:23 +0900

    Moved test initialization to outer describe block

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 const { expect } = chai;
 
-describe("vDOM implementation", () => {
+describe("cc4-sprint.vdom", () => {
   let aProps, divElement, spanElement, textElement, seedElements;
 
   beforeEach(() => {
@@ -24,262 +24,274 @@ describe("vDOM implementation", () => {
     );
   });
 
-  // we have some spec titles to help you get started
+  describe("vDOM implementation", () => {
+    // we have some spec titles to help you get started
 
-  describe("createVDOM function", () => {
-    it("should have a function called 'createVDOM'", () => {
-      expect(createVDOM).to.be.a("function");
+    describe("createVDOM function", () => {
+      it("should have a function called 'createVDOM'", () => {
+        expect(createVDOM).to.be.a("function");
+      });
+
+      it("should return an object with type, props, and children properties", () => {
+        expect(spanElement)
+          .to.be.an("object")
+          .that.has.all.keys("type", "props", "children");
+      });
+
+      it("should return a string for type", () => {
+        expect(spanElement.type).to.be.a("string");
+      });
+
+      it("should return an array of children objects", () => {
+        expect(spanElement.children).to.be.an("array");
+      });
+
+      it("should return a object of props", () => {
+        expect(spanElement.props).to.be.an("object");
+      });
+
+      it("should return an array of grandchildren objects", () => {
+        const grandChildren = seedElements.children[0].children;
+
+        expect(grandChildren).to.be.an("array");
+      });
+
+      it("should return an array of great-grandchildren objects", () => {
+        const greatGrandChildren =
+          seedElements.children[0].children[1].children;
+
+        expect(greatGrandChildren).to.be.an("array");
+      });
+
+      it("should have a string value to represent a text node when given a string (aka text element)", () => {
+        const textNode = divElement.children[0];
+        expect(textNode).to.be.a("string");
+      });
     });
 
-    it("should return an object with type, props, and children properties", () => {
-      expect(spanElement)
-        .to.be.an("object")
-        .that.has.all.keys("type", "props", "children");
+    describe("createElement function", () => {
+      let result;
+
+      beforeEach(() => {
+        // create your own seed elements or use the ones created above!
+        result = createElement(/* your seed elements */);
+      });
+
+      it("should have a function called createElement", () => {
+        expect(createElement).to.be.a("function");
+      });
+
+      it("should return an HTML Element", () => {
+        expect(result.tagName).to.equal("A");
+      });
+
+      it("should convert childNodes to HTML", () => {
+        /* Clues:
+        |* child elements should NOT include text nodes
+        |* BUT child nodes SHOULD include text nodes
+        |* This may be important for testing...
+        |* ...ok, it obviously is, so take this clue into account.
+        */
+      });
+
+      it("should convert grand childNodes to HTML", () => {
+        // see the clue above for this
+      });
+
+      it("should convert props to attributes", () => {});
     });
 
-    it("should return a string for type", () => {
-      expect(spanElement.type).to.be.a("string");
-    });
+    describe("updateElement function", () => {
+      function resetParent() {
+        target.innerHTML = "";
+        const childA = document.createElement("a");
+        const childP = document.createElement("p");
+        const grandchildFont = document.createElement("font");
+        childP.appendChild(grandchildFont);
+        target.append(childA, childP);
+      }
 
-    it("should return an array of children objects", () => {
-      expect(spanElement.children).to.be.an("array");
-    });
+      beforeEach(() => {
+        resetParent();
+      });
 
-    it("should return a object of props", () => {
-      expect(spanElement.props).to.be.an("object");
-    });
-
-    it("should return an array of grandchildren objects", () => {
-      const grandChildren = seedElements.children[0].children;
-
-      expect(grandChildren).to.be.an("array");
-    });
-
-    it("should return an array of great-grandchildren objects", () => {
-      const greatGrandChildren = seedElements.children[0].children[1].children;
-
-      expect(greatGrandChildren).to.be.an("array");
-    });
-
-    it("should have a string value to represent a text node when given a string (aka text element)", () => {
-      const textNode = divElement.children[0];
-      expect(textNode).to.be.a("string");
-    });
-  });
-
-  describe("createElement function", () => {
-    let result;
-
-    beforeEach(() => {
-      // create your own seed elements or use the ones created above!
-      result = createElement(/* your seed elements */);
-    });
-
-    it("should have a function called createElement", () => {
-      expect(createElement).to.be.a("function");
-    });
-
-    it("should return an HTML Element", () => {
-      expect(result.tagName).to.equal("A");
-    });
-
-    it("should convert childNodes to HTML", () => {
-      /* Clues:
-      |* child elements should NOT include text nodes
-      |* BUT child nodes SHOULD include text nodes
-      |* This may be important for testing...
-      |* ...ok, it obviously is, so take this clue into account.
-      */
-    });
-
-    it("should convert grand childNodes to HTML", () => {
-      // see the clue above for this
-    });
-
-    it("should convert props to attributes", () => {});
-  });
-
-  describe("updateElement function", () => {
-    function resetParent() {
-      target.innerHTML = "";
-      const childA = document.createElement("a");
-      const childP = document.createElement("p");
-      const grandchildFont = document.createElement("font");
-      childP.appendChild(grandchildFont);
-      target.append(childA, childP);
-    }
-
-    beforeEach(() => {
-      resetParent();
-    });
-
-    let target = document.getElementById("tes-div");
-    let oldNode = createVDOM(
-      "div",
-      { id: "tes-div", style: "display: none;" },
-      createVDOM("a"),
-      createVDOM("p", null, createVDOM("font"))
-    );
-
-    it("updateElement should be a function", () => {
-      expect(updateElement).to.be.a("function");
-    });
-
-    it("should update target element with new nodes", () => {
-      let newNodeAddToEnd = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("a"),
-        createVDOM("p", null, createVDOM("font")),
-        createVDOM("span")
-      );
-
-      updateElement(target, newNodeAddToEnd, oldNode);
-
-      expect(target.childNodes.length).to.equal(3);
-      expect(target.childNodes[0].nodeName).to.equal("A");
-      expect(target.childNodes[1].nodeName).to.equal("P");
-      expect(target.childNodes[2].nodeName).to.equal("SPAN");
-    });
-
-    it("should update target element with new nodes", () => {
-      let newNodeAddToBeginning = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("span"),
-        createVDOM("a"),
-        createVDOM("p", null, createVDOM("font"))
-      );
-      updateElement(target, newNodeAddToBeginning, oldNode);
-
-      expect(target.childNodes.length).to.equal(3);
-
-      expect(target.childNodes[0].nodeName).to.equal("SPAN");
-      expect(target.childNodes[1].nodeName).to.equal("A");
-      expect(target.childNodes[2].nodeName).to.equal("P");
-    });
-
-    it("should delete old nodes", () => {
-      let nodeRemoveFromMiddle = createVDOM(
+      let target = document.getElementById("tes-div");
+      let oldNode = createVDOM(
         "div",
         { id: "tes-div", style: "display: none;" },
-        createVDOM("p", null, createVDOM("font"))
-      );
-
-      updateElement(target, nodeRemoveFromMiddle, oldNode);
-
-      expect(target.childNodes.length).to.equal(1);
-      expect(target.childNodes[0].nodeName).to.equal("P");
-    });
-
-    it("should update target element with new nodes and texts", () => {
-      let newNodeAddText = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("a"),
-        "Hello World",
-        createVDOM("p", null, createVDOM("font"))
-      );
-
-      updateElement(target, newNodeAddText, oldNode);
-
-      expect(target.childNodes.length).to.equal(3);
-      expect(target.childNodes[0].nodeName).to.equal("A");
-      expect(target.childNodes[1].textContent).to.equal("Hello World");
-      expect(target.childNodes[2].nodeName).to.equal("P");
-    });
-
-    it("should update target element with new grand children nodes", () => {
-      let newGrandChildrenNodes = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("a"),
-        createVDOM("p", null, createVDOM("font"), createVDOM("a", null, "Bar"))
-      );
-
-      updateElement(target, newGrandChildrenNodes, oldNode);
-
-      expect(target.childNodes.length).to.equal(2);
-      expect(target.childNodes[0].nodeName).to.equal("A");
-      expect(target.childNodes[1].nodeName).to.equal("P");
-      expect(target.childNodes[1].childNodes.length).to.equal(2);
-      expect(target.childNodes[1].childNodes[0].nodeName).to.equal("FONT");
-      expect(target.childNodes[1].childNodes[1].nodeName).to.equal("A");
-      expect(target.childNodes[1].childNodes[1].childNodes.length).to.equal(1);
-      expect(
-        target.childNodes[1].childNodes[1].childNodes[0].textContent
-      ).to.equal("Bar");
-    });
-
-    it("should update target element with new text for grand child node", () => {
-      let newTextToGrandChildNode = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("a"),
-        createVDOM("p", null, createVDOM("font", null, "Foo"))
-      );
-
-      updateElement(target, newTextToGrandChildNode, oldNode);
-
-      expect(target.childNodes[1].childNodes[0].nodeName).to.equal("FONT");
-      expect(target.childNodes[1].childNodes[0].childNodes.length).to.equal(1);
-      expect(
-        target.childNodes[1].childNodes[0].childNodes[0].textContent
-      ).to.equal("Foo");
-    });
-
-    it("should update attributes of target element", () => {
-      let changeAttributesAddToTarget = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: block;",
-        },
         createVDOM("a"),
         createVDOM("p", null, createVDOM("font"))
       );
 
-      updateElement(target, changeAttributesAddToTarget, oldNode);
+      it("updateElement should be a function", () => {
+        expect(updateElement).to.be.a("function");
+      });
 
-      let newTarget = document.getElementById("tes-div");
+      it("should update target element with new nodes", () => {
+        let newNodeAddToEnd = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("a"),
+          createVDOM("p", null, createVDOM("font")),
+          createVDOM("span")
+        );
 
-      expect(newTarget.attributes.length).to.equal(2);
-      expect(newTarget.getAttribute("id")).to.equal("tes-div");
-      expect(newTarget.getAttribute("style")).to.equal("display: block;");
-    });
+        updateElement(target, newNodeAddToEnd, oldNode);
 
-    it("should update attributes of children of target element", () => {
-      let newAttributesAddToChild = createVDOM(
-        "div",
-        {
-          id: "tes-div",
-          style: "display: none;",
-        },
-        createVDOM("a"),
-        createVDOM("p", { class: "baz" }, createVDOM("font"))
-      );
+        expect(target.childNodes.length).to.equal(3);
+        expect(target.childNodes[0].nodeName).to.equal("A");
+        expect(target.childNodes[1].nodeName).to.equal("P");
+        expect(target.childNodes[2].nodeName).to.equal("SPAN");
+      });
 
-      updateElement(target, newAttributesAddToChild, oldNode);
+      it("should update target element with new nodes", () => {
+        let newNodeAddToBeginning = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("span"),
+          createVDOM("a"),
+          createVDOM("p", null, createVDOM("font"))
+        );
+        updateElement(target, newNodeAddToBeginning, oldNode);
 
-      expect(target.childNodes.length).to.equal(2);
-      expect(target.childNodes[0].attributes.length).to.equal(0);
-      expect(target.childNodes[1].attributes.length).to.equal(1);
-      expect(target.childNodes[1].getAttribute("class")).to.equal("baz");
+        expect(target.childNodes.length).to.equal(3);
+
+        expect(target.childNodes[0].nodeName).to.equal("SPAN");
+        expect(target.childNodes[1].nodeName).to.equal("A");
+        expect(target.childNodes[2].nodeName).to.equal("P");
+      });
+
+      it("should delete old nodes", () => {
+        let nodeRemoveFromMiddle = createVDOM(
+          "div",
+          { id: "tes-div", style: "display: none;" },
+          createVDOM("p", null, createVDOM("font"))
+        );
+
+        updateElement(target, nodeRemoveFromMiddle, oldNode);
+
+        expect(target.childNodes.length).to.equal(1);
+        expect(target.childNodes[0].nodeName).to.equal("P");
+      });
+
+      it("should update target element with new nodes and texts", () => {
+        let newNodeAddText = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("a"),
+          "Hello World",
+          createVDOM("p", null, createVDOM("font"))
+        );
+
+        updateElement(target, newNodeAddText, oldNode);
+
+        expect(target.childNodes.length).to.equal(3);
+        expect(target.childNodes[0].nodeName).to.equal("A");
+        expect(target.childNodes[1].textContent).to.equal("Hello World");
+        expect(target.childNodes[2].nodeName).to.equal("P");
+      });
+
+      it("should update target element with new grand children nodes", () => {
+        let newGrandChildrenNodes = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("a"),
+          createVDOM(
+            "p",
+            null,
+            createVDOM("font"),
+            createVDOM("a", null, "Bar")
+          )
+        );
+
+        updateElement(target, newGrandChildrenNodes, oldNode);
+
+        expect(target.childNodes.length).to.equal(2);
+        expect(target.childNodes[0].nodeName).to.equal("A");
+        expect(target.childNodes[1].nodeName).to.equal("P");
+        expect(target.childNodes[1].childNodes.length).to.equal(2);
+        expect(target.childNodes[1].childNodes[0].nodeName).to.equal("FONT");
+        expect(target.childNodes[1].childNodes[1].nodeName).to.equal("A");
+        expect(target.childNodes[1].childNodes[1].childNodes.length).to.equal(
+          1
+        );
+        expect(
+          target.childNodes[1].childNodes[1].childNodes[0].textContent
+        ).to.equal("Bar");
+      });
+
+      it("should update target element with new text for grand child node", () => {
+        let newTextToGrandChildNode = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("a"),
+          createVDOM("p", null, createVDOM("font", null, "Foo"))
+        );
+
+        updateElement(target, newTextToGrandChildNode, oldNode);
+
+        expect(target.childNodes[1].childNodes[0].nodeName).to.equal("FONT");
+        expect(target.childNodes[1].childNodes[0].childNodes.length).to.equal(
+          1
+        );
+        expect(
+          target.childNodes[1].childNodes[0].childNodes[0].textContent
+        ).to.equal("Foo");
+      });
+
+      it("should update attributes of target element", () => {
+        let changeAttributesAddToTarget = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: block;",
+          },
+          createVDOM("a"),
+          createVDOM("p", null, createVDOM("font"))
+        );
+
+        updateElement(target, changeAttributesAddToTarget, oldNode);
+
+        let newTarget = document.getElementById("tes-div");
+
+        expect(newTarget.attributes.length).to.equal(2);
+        expect(newTarget.getAttribute("id")).to.equal("tes-div");
+        expect(newTarget.getAttribute("style")).to.equal("display: block;");
+      });
+
+      it("should update attributes of children of target element", () => {
+        let newAttributesAddToChild = createVDOM(
+          "div",
+          {
+            id: "tes-div",
+            style: "display: none;",
+          },
+          createVDOM("a"),
+          createVDOM("p", { class: "baz" }, createVDOM("font"))
+        );
+
+        updateElement(target, newAttributesAddToChild, oldNode);
+
+        expect(target.childNodes.length).to.equal(2);
+        expect(target.childNodes[0].attributes.length).to.equal(0);
+        expect(target.childNodes[1].attributes.length).to.equal(1);
+        expect(target.childNodes[1].getAttribute("class")).to.equal("baz");
+      });
     });
   });
 });
