f8204adef0ff563df980ba7a8c72939399a1f8cb
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-12 11:29:08 +0900

    Added jsDoc for createVDOM and createElement

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+/**
+ * Create and return a virtual DOM object from the given type, properties, and children.
+ * @param {String} type the name of the HTMLElement
+ * @param {Object} props an object of element attributes
+ * @param {String|Object} children an array of Strings or VDOM objects that are children of this HTMLElement.
+ * @returns {Object}
+ */
 // eslint-disable-next-line no-unused-vars
 function createVDOM(type, props, ...children) {}
 
+/**
+ * Create and returns an HTML Element given a VDOM object
+ * @param {Object} node
+ * @returns {Object} HTMLElement
+ */
 // eslint-disable-next-line no-unused-vars
 function createElement(node) {}
 
